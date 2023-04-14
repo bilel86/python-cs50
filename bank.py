18855560cded9214b2181cab7352245ad9398290
bilel86 <bileljaouadi86@gmail.com>
2023-04-15 00:18:12 +0100

    ADD bank.py

---
+++ bank.py
@@ -0,0 +1,24 @@
+  GNU nano 7.1                        bank.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                [ Read 0 lines ]
+^G Help      ^O Write Out ^W Where Is  ^K Cut       ^T Execute   ^C Location
+^X Exit      ^R Read File ^\ Replace   ^U Paste     ^J Justify   ^/ Go To Line
